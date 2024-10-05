8cf98ae742ede0b7c30fa057472f8a206c897e27
Marc Evans <marcaevans89@icloud.com>
2024-10-05 14:59:57 +0100

    Initial Commit

---
+++ CoffeeReminder/CoffeeReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeReminderApp.swift
+//  CoffeeReminder
+//
+//  Created by Marc Evans on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
